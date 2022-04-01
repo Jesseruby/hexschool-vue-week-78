7f29d682522b5843467ea9a38344b91d520cdca9
Jesseruby <47548372+Jesseruby@users.noreply.github.com>
2022-04-01 10:37:03 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,8 @@ module.exports = defineConfig({
   transpileDependencies: true,
 });
 
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production'
-//     ? '/hexschool-vue-week-6/' //資料夾路徑，儲存庫名稱
-//     : '/'
-// }
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/hexschool-vue-week-78/' //資料夾路徑，gh-page 儲存庫名稱
+    : '/'
+};
